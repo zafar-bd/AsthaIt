4000d0d575507c77e8c36fe59535d6e140deeda7
Md Abu Zafar <mazafar.bd@gmail.com>
2021-04-20 13:48:26 +0600

    Add project files.

---
+++ src/CommonPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CommonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Common Pattern";
+            //const string input = "contest.txt context.txt";
+            const string input = "config.sys config.inf configures";
+
+            var outPut = Execute(input);
+            Console.WriteLine(outPut);
+            Console.ReadLine();
+        }
+
+        private static string Execute(string input)
+        {
+            string[] splittedInputs = input.Split(' ');
+            string outPut = string.Empty;
+            string firstSampleValue = splittedInputs[0];
+
+            for (int i = 0; i < firstSampleValue.Length; i++)
+            {
+                bool isMatched = true;
+                for (int j = 1; j < splittedInputs.Length; j++)
+                {
+                    string firstVal = splittedInputs[j];
+                    if (firstSampleValue[i] != firstVal[i])
+                    {
+                        isMatched = false;
+                        break;
+                    }
+                }
+
+                if (!isMatched)
+                {
+                    outPut += '?';
+                }
+                else
+                {
+                    outPut += firstSampleValue[i];
+                }
+            }
+
+            return outPut;
+        }
+    }
+}
---
+++ src/Locker/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Locker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Locker";
+            const uint numberOfLockers = 9;
+            var lockerArray = Execute(numberOfLockers);
+
+            Console.WriteLine($"{lockerArray[0]}");
+            Console.ReadLine();
+        }
+
+        private static int[] Execute(uint numberOfLockers)
+        {
+            int[] lockerArray = new int[numberOfLockers];
+
+            for (int i = 0; i < numberOfLockers; i++)
+            {
+                lockerArray[i] = i + 1;
+            }
+
+            int n = 1;
+            while (lockerArray.Length > 1)
+            {
+                int[] unlockedLockers = new int[numberOfLockers];
+                for (int i = 0; i < lockerArray.Length; i++)
+                {
+                    int index = i == 0 ? 0 : (n + 1) * i;
+                    if (index >= lockerArray.Length)
+                    {
+                        break;
+                    }
+
+                    unlockedLockers[i] = lockerArray[index];
+                }
+
+                lockerArray = lockerArray.Except(unlockedLockers).ToArray();
+                n++;
+            }
+
+            return lockerArray;
+        }
+    }
+}
